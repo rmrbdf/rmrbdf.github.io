c22769f60680b8b637f0169ae2b2cfda5d4b1eb3
rmrbdf <81110343+rmrbdf@users.noreply.github.com>
2021-03-21 13:10:54 -0600

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,18 @@
+/*  Inicializa Firebase con la
+ * configuración del proyecto.
+ * Revisa la configuración en tu
+ * servidor de Firebase y cópiala
+ * aquí sustituyendo los
+ * asteriscos. Los campos deben
+ * quedar igual que en tu
+ * servidor. */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyAJgnkfV60cI3782eCRC6Q47Uw0l6G9ydU",
+  authDomain: "gilpgbdx.firebaseapp.com",
+  databaseURL: "https://gilpgbdx.firebaseio.com",
+  projectId: "gilpgbdx",
+  storageBucket: "gilpgbdx.appspot.com",
+  messagingSenderId: "255502468565",
+  appId: "1:255502468565:web:424d9517a4e10deb5aa95c"
+});
